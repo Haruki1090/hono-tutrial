3a93180fd6591398bca9a4039744476043c25386
Haruki1090 <haruharu.com.109607@gmail.com>
2024-06-26 16:07:43 +0900

    🔥 不要コード削除

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import { Hono } from "hono";
-import { prettyJSON } from "hono/pretty-json";
 import posts from "./blogs/blogs";
 
 const app = new Hono();
 app.route("/posts", posts);
-
-// app.use("*", prettyJSON());
 
 export default app;
