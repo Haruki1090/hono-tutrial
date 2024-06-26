5a4fceebe7fa3aa704eebeb2394c9302579a9aca
Haruki1090 <haruharu.com.109607@gmail.com>
2024-06-26 16:05:15 +0900

    ♻️ リファクタリング

--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,10 @@
 import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
+import posts from "./blogs/blogs";
 
 const app = new Hono();
-
-let blogPosts = [
-  {
-    id: 1,
-    title: "Blog1",
-    content: "Blog1 content",
-  },
-  {
-    id: 2,
-    title: "Blog2",
-    content: "Blog2 content",
-  },
-  {
-    id: 3,
-    title: "Blog3",
-    content: "Blog3 content",
-  },
-];
+app.route("/posts", posts);
 
 // app.use("*", prettyJSON());
-
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
-
-app.get("/posts", (c) => c.json({ posts: blogPosts }));
-
-app.get("/posts/:id", (c) => {
-  const id = Number(c.req.param("id"));
-  const post = blogPosts.find((p) => p.id === id);
-
-  if (post) {
-    return c.json(post);
-  } else {
-    return c.json({ message: "not found this page" }, 404);
-  }
-});
-
-app.post("/posts", async (c) => {
-  const { title, content } = await c.req.json<{
-    title: string;
-    content: string;
-  }>();
-  const newPost = { id: Number(blogPosts.length + 1), title, content };
-  blogPosts = [...blogPosts, newPost];
-  return c.json(newPost, 201);
-});
-
-app.put("/posts/:id", async (c) => {
-  const id = Number(c.req.param("id"));
-  const postIndex = blogPosts.findIndex((p) => p.id === id);
-
-  if (postIndex === -1) {
-    return c.json({ message: "not found this page" }, 404);
-  }
-
-  const { title, content } = await c.req.json<{
-    title: string;
-    content: string;
-  }>();
-  blogPosts[postIndex] = { ...blogPosts[postIndex], title, content };
-
-  return c.json(blogPosts[postIndex]);
-});
-
-app.delete("/posts/:id", async (c) => {
-  const id = Number(c.req.param("id"));
-  const postIndex = blogPosts.findIndex((p) => p.id === id);
-
-  if (postIndex === -1) {
-    return c.json({ message: "not found this page" }, 404);
-  }
-
-  blogPosts = blogPosts.filter((p) => p.id !== id);
-
-  return c.json({ message: "Blog Post deleted" });
-});
 
 export default app;
